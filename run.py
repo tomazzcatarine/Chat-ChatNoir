b90e76951a588ea73090865154238cb50bebd8b2
Catarine Tomaz <165063607+tomazzcatarine@users.noreply.github.com>
2025-08-15 00:17:19 -0300

    Update run.py

--- run.py
+++ run.py
@@ -63,16 +63,16 @@ def logout():
 
 @app.route('/chat', methods=['POST', 'GET'])
 def home():
-    return render_template('chat.html')
+    if 'username' not in session:
+        return redirect(url_for('login'))
+    return render_template('chat.html', username=session['username'])
+
 
 @io.on('sendMessage')
 def send_message_handler(msg):
     messages.append(msg) 
     emit('getMessage', msg, broadcast=True)
 
-@io.on('message')
-def message_handler(msg):
-    send(messages)
-
 if __name__ == '__main__':    
     io.run(app, host="0.0.0.0", port=5000, debug=True)
+
